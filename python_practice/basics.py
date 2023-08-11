8c2def053d160d59f624a8c28938fe13d319076e
Ignacio Pina <ignacio.pina.87@gmail.com>
2023-08-11 04:39:21 -0500

    Add today's practice

--- python_practice/basics.py
+++ python_practice/basics.py
@@ -1,6 +1,7 @@
 day_hours = 24
 week_days = 7
+hour_minutes = 60
 
-week_hours = day_hours * week_days
+week_minutes = (hour_minutes * day_hours) * week_days
 
-print(week_hours)
+print(week_minutes)
